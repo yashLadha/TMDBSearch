1a457515afc233ed8c9dd008ea177a9f34d870d9
Yash Ladha <yashladhapankajladha123@gmail.com>
2018-11-10 17:20:11 +0530

    Updates

---
+++ precache-manifest.c8312ac464705317a737508b80519ff0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3d371b390c32bcd397f",
+    "url": "/TMDBSearch/static/js/main.f3d371b3.chunk.js"
+  },
+  {
+    "revision": "6985e3e2394dc07985ee",
+    "url": "/TMDBSearch/static/js/1.6985e3e2.chunk.js"
+  },
+  {
+    "revision": "3ad4ee9c7439eb728481",
+    "url": "/TMDBSearch/static/js/runtime~main.3ad4ee9c.js"
+  },
+  {
+    "revision": "3b4c96ac5f581903d69476246fd4c957",
+    "url": "/TMDBSearch/index.html"
+  }
+];
